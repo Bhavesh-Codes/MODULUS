32e304bea22bb8a1c24ce68451ff800b3b1665f1
Bhavesh-Codes <bhaveshjalalbisht@gmail.com>
2026-04-27 18:49:48 +0530

    deploy

--- components/landing/LandingNav.tsx
+++ components/landing/LandingNav.tsx
@@ -19,9 +19,8 @@ export default function LandingNav() {
 
   return (
     <motion.nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 w-full ${
-        isScrolled ? "bg-white border-b-2 border-black" : "bg-transparent border-transparent"
-      }`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 w-full ${isScrolled ? "bg-white border-b-2 border-black" : "bg-transparent border-transparent"
+        }`}
       initial={false}
       animate={{
         backgroundColor: isScrolled ? "#FFFFFF" : "rgba(255, 255, 255, 0)",
